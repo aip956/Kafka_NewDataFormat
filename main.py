4fa776914235eb638f23bb633297185ec2c843ab
Anthea Ip <antheaip964@gmail.com>
2024-08-27 20:05:55 -0400

    Adding team messages

--- main.py
+++ main.py
@@ -157,6 +157,14 @@ event_team_mapping = {
         "person_fell": ["Security", "Waiters"]
     }
 
+# For getting events by team
+# consumed_messages = []
+security_messages = []
+clean_up_messages = []
+catering_messages = []
+officiant_messages = []
+waiters_messages = []
+
 
 @app.post("/events")
 async def receive_event(event: Event):
@@ -191,6 +199,18 @@ async def dispatch_event(event):
                 logger.info(f"Dispatched {event_type} event {event_id} with team {team_name}")
                 if team:
                     logger.info(f"Dispatched {event_type} event {event_id} by team {team_name}.")
+                    # Store event in event type list
+                    if team_name == "Security":
+                        security_messages.append(event)
+                    elif team_name == "Clean_Up":
+                        clean_up_messages.append(event)
+                    elif team_name == "Catering":
+                        catering_messages.append(event)
+                    elif team_name == "Officiant":
+                        officiant_messages.append(event)
+                    elif team_name == "Waiters":
+                        waiters_messages.append(event)
+
                     if await team.assign_event(event):
                         event_handled = True
                         break
@@ -272,6 +292,31 @@ async def get_events():
     # return {"events": events}
     return events
 
+# For the teams' messages
+@app.get("/security_messages")
+def get_security_messages():
+    logger.info(f"Returning security messages: {security_messages}")
+    return {"security messages": security_messages}
+
+@app.get("/clean_up_messages")
+def get_clean_up_messages():
+    logger.info(f"Returning clean_up messages: {clean_up_messages}")
+    return {"clean_up messages": clean_up_messages}
+
+@app.get("/catering_messages")
+def get_catering_messages():
+    logger.info(f"Returning catering messages: {catering_messages}")
+    return {"catering messages": catering_messages}
+
+@app.get("/officiant_messages")
+def get_officiant_messages():
+    logger.info(f"Returning officiant messages: {officiant_messages}")
+    return {"officiant messages": officiant_messages}
+
+@app.get("/waiters_messages")
+def get_waiters_messages():
+    logger.info(f"Returning waiter messages: {waiters_messages}")
+    return {"waiters messages": waiters_messages}
 
 
 
@@ -283,224 +328,3 @@ if __name__ == "__main__":
 
 
 
-# For getting events by team
-# consumed_messages = []
-# security_messages = []
-# clean_up_messages = []
-# catering_messages = []
-# officiant_messages = []
-# waiters_messages = []
-
-# @app.on_event("startup")
-# async def on_startup():
-#     try:
-#         await producer.start()
-#         logger.info("Kafka producer started")
-#         asyncio.create_task(consume_all())
-#         asyncio.create_task(consume_security())
-#         asyncio.create_task(consume_clean_up())
-#         asyncio.create_task(consume_catering())
-#         asyncio.create_task(consume_officiant())
-#         asyncio.create_task(consume_waiters())
-#         logger.info("Kafka consumer tasks created")
-#     except errors.KafkaConnectionError as e:
-#         logger.error(f"Kafka connection error: {e}")
-
-
-
-# @app.post("/send/{topic}")
-# async def produce(topic: str, message: str):
-#     try:
-#         await producer.send_and_wait(topic, message.encode('utf-8'))
-#         logger.info(f"Produced message: {message} to topic: {topic}")
-#         return {"message": "Message sent successfully"}
-#     except errors.KafkaConnectionError as e:
-#         logger.error(f"Kafka connection error: {e}")
-#         return {"message": "Failed to send message"}
-
-# async def consume_all():
-#     consumer = AIOKafkaConsumer(
-#         *topics, # Unpack all topic names
-#         loop=loop,
-#         bootstrap_servers=KAFKA_BOOTSTRAP_SERVER,
-#         group_id= "AllConsumers",
-#         session_timeout_ms = 10000, # 6 seconds
-#         heartbeat_interval_ms = 3000, # 2 seconds
-#     )
-    # await consumer.start()
-    # try:
-    #     async for msg in consumer:
-    #         message = msg.value.decode('utf-8')
-    #         topic = msg.topic
-    #         consumed_messages.append({"topic": topic, "message": message})
-    #         logger.info(f"Consumed_all message: {message} from topic: {topic}")
-    # except Exception as e:
-    #     logger.error(f"Error occurred: {e}")
-    # finally:
-    #     await consumer.stop()
-
-# async def consume_security():
-#     consumer = AIOKafkaConsumer(
-#         *SECURITY_TOPICS,
-#         loop=loop,
-#         bootstrap_servers=KAFKA_BOOTSTRAP_SERVER,
-#         group_id="Security")
-#     await consumer.start()
-#     try:
-#         async for msg in consumer:
-#             message = msg.value.decode('utf-8')
-#             topic = msg.topic
-#             security_messages.append({"topic": topic, "message": message})
-#             logger.info(f"Security message: {message} from topic: {topic}")
-#     except Exception as e:
-#         logger.error(f"Error occurred: {e}")
-#     finally:
-#         await consumer.stop()
-
-# async def consume_clean_up():
-#     consumer = AIOKafkaConsumer(
-#         *CLEAN_UP_TOPICS,
-#         loop=loop,
-#         bootstrap_servers=KAFKA_BOOTSTRAP_SERVER,
-#         group_id="Clean_Up")
-#     await consumer.start()
-#     try:
-#         async for msg in consumer:
-#             message = msg.value.decode('utf-8')
-#             topic = msg.topic
-#             clean_up_messages.append({"topic": topic, "message": message})
-#             logger.info(f"Clean_Up message: {message} from topic: {topic}")
-#     except Exception as e:
-#         logger.error(f"Error occurred: {e}")
-#     finally:
-#         await consumer.stop()
-
-
-# async def consume_catering():
-#     consumer = AIOKafkaConsumer(
-#         *CATERING_TOPICS, #Sub in KAFA_TOPIC later
-#         loop=loop,
-#         bootstrap_servers=KAFKA_BOOTSTRAP_SERVER,
-#         group_id="Catering")
-#     await consumer.start()
-#     try:
-#         async for msg in consumer:
-#             message = msg.value.decode('utf-8')
-#             topic = msg.topic
-#             catering_messages.append({"topic": topic, "message": message})
-#             logger.info(f"Catering message: {message} from topic: {topic}")
-#     except Exception as e:
-#         logger.error(f"Error occurred: {e}")
-#     finally:
-#         await consumer.stop()
-
-# async def consume_officiant():
-#     consumer = AIOKafkaConsumer(
-#         *OFFICIANT_TOPICS,
-#         loop=loop,
-#         bootstrap_servers=KAFKA_BOOTSTRAP_SERVER,
-#         group_id="Officiant")
-#     await consumer.start()
-#     try:
-#         async for msg in consumer:
-#             message = msg.value.decode('utf-8')
-#             topic = msg.topic
-#             officiant_messages.append({"topic": topic, "message": message})
-#             logger.info(f"Officiant message: {message} from topic: {topic}")
-#     except Exception as e:
-#         logger.error(f"Error occurred: {e}")
-#     finally:
-#         await consumer.stop()
-
-# async def consume_waiters():
-#     consumer = AIOKafkaConsumer(
-#         *WAITERS_TOPICS,
-#         loop=loop,
-#         bootstrap_servers=KAFKA_BOOTSTRAP_SERVER,
-#         group_id="Waiters")
-#     await consumer.start()
-#     try:
-#         async for msg in consumer:
-#             message = msg.value.decode('utf-8')
-#             topic = msg.topic
-#             waiters_messages.append({"topic": topic, "message": message})
-#             logger.info(f"Waiting message: {message} from topic: {topic}")
-#     except Exception as e:
-#         logger.error(f"Error occurred: {e}")
-#     finally:
-#         await consumer.stop()
-
-
-# @app.get("/messages")
-# def get_messages():
-#     logger.info(f"Returning consumed messages: {consumed_messages}")
-#     return {"All messages": consumed_messages}
-
-# @app.get("/security_messages")
-# def get_security_messages():
-#     logger.info(f"Returning security messages: {security_messages}")
-#     return {"security messages": security_messages}
-
-# @app.get("/clean_up_messages")
-# def get_clean_up_messages():
-#     logger.info(f"Returning clean_up messages: {clean_up_messages}")
-#     return {"clean_up messages": clean_up_messages}
-
-# @app.get("/catering_messages")
-# def get_catering_messages():
-#     logger.info(f"Returning catering messages: {catering_messages}")
-#     return {"catering messages": catering_messages}
-
-# @app.get("/officiant_messages")
-# def get_officiant_messages():
-#     logger.info(f"Returning officiant messages: {officiant_messages}")
-#     return {"officiant messages": officiant_messages}
-
-# @app.get("/waiters_messages")
-# def get_waiters_messages():
-#     logger.info(f"Returning waiter messages: {waiters_messages}")
-#     return {"waiters messages": waiters_messages}
-
-
-
-
-# Kafka producer and consumer setup 
-
-
-
-    # Event handler function
-# async def handle_event(event):
-#     global stress_level
-#     start_time = datetime.now()
-#     timeout = PRIORITY_TIMEFRAMES[event['priority']]
-
-#     # Simulate processing
-#     processing_time = random.randint(1, 20)
-#     await asyncio.sleep(processing_time)
-
-#     elapsed_time = (datetime.now() - start_time).seconds
-#     if elapsed_time > timeout:
-#         stress_level += 1
-#         logger.info(f"Event {event['event_type']} not handled in time. Stress level increased")
-#     else:
-#         logger.info(f"Event {event['event_type']} handled in time")
-
-#     # Log event
-#     events.append({"event": event, "handled": elapsed_time <= timeout})
-
-
-# # Kafka consumer function
-# async def consume_events():
-#     consumer = AIOKafkaConsumer(
-#         TOPIC,
-#         bootstrap_servers=KAFKA_BOOTSTRAP_SERVER,
-#         group_id = "wedding_group"
-#     )
-#     await consumer.start()
-#     try:
-#         async for msg in consumer:
-#             event = json.loads(msg.value.decode("utf-8"))
-#             await handle_event(event)
-#             logger.info(f"Consumed and processed event: {event['event_type']}")
-#     finally:
-#         await consumer.stop()
